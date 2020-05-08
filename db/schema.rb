3e02610f678591f92f39edaa66c5bb8dfd2cb165
Gunar Kononov <gunar.kononov@gmail.com>
2020-05-08 18:43:33 +0300

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_07_184101) do
+ActiveRecord::Schema.define(version: 2020_05_08_140748) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,15 +30,11 @@ ActiveRecord::Schema.define(version: 2020_05_07_184101) do
     t.string "title"
     t.text "plot"
     t.integer "index"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
     t.text "plot"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "purchases", force: :cascade do |t|
@@ -57,8 +53,6 @@ ActiveRecord::Schema.define(version: 2020_05_07_184101) do
     t.string "title"
     t.text "plot"
     t.integer "index"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
