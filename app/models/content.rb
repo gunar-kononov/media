42f66b78b1615818de6a8e90eb6eccfce8c4f793
Gunar Kononov <gunar.kononov@gmail.com>
2020-05-11 14:42:11 +0300

    Touch parent when child is updated to invalidate parent cache key, if any

--- app/models/content.rb
+++ app/models/content.rb
@@ -5,7 +5,7 @@ class Content < ApplicationRecord
   has_many :purchases, inverse_of: :content
   has_many :users, through: :purchases
   belongs_to :media, polymorphic: true, required: true, dependent: :destroy
-  belongs_to :parent, class_name: 'Content', foreign_key: :content_id, optional: true
+  belongs_to :parent, class_name: 'Content', foreign_key: :content_id, touch: true, optional: true
   has_many :children, class_name: 'Content', foreign_key: :content_id, dependent: :destroy
 
   validates :purchasable, inclusion: { in: [true, false] }
